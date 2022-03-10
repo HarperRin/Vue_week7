308c21df17fd865c2527e6cf6b61a5bccc495bbd
Wendy <wink899@gmail.com>
2022-03-11 00:12:09 +0800

    deploy

---
+++ js/707.bafc9960.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvue_week7"]=self["webpackChunkvue_week7"]||[]).push([[707],{7707:function(e,n,t){t.r(n),t.d(n,{default:function(){return k}});var r=t(6252);const u={class:"index"},c=(0,r._)("h2",{class:"text-center mt-5"},"前台首頁",-1),s=[c];function a(e,n,t,c,a,i){return(0,r.wg)(),(0,r.iD)("div",u,s)}var i={name:"IndexView"},d=t(3744);const f=(0,d.Z)(i,[["render",a]]);var k=f}}]);
+//# sourceMappingURL=707.bafc9960.js.map
